9ef886deb4915f8e6612e08895cb62fc54a8cb69
Csc32 <sanzonettycar@gmail.com>
2022-08-04 15:34:12 -0400

    Upgrade the function

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import pdfplumber, os, pandas
 def createBingo(file, path, fileName = "serie", number_of_page = 0, ):
     if not(file) or not(path):
         print("Error your not provide the file or path")
-
+        return
     print(file)
     
     data = pdfplumber.open(file).pages[number_of_page].extract_tables()
@@ -14,18 +14,23 @@ def createBingo(file, path, fileName = "serie", number_of_page = 0, ):
         dataFile = f"{fileName}{str(i)}.csv"
 
         if os.listdir().count(dataFile):
+
             os.remove(dataFile)
 
-            print(f"Remove the file {fileName + str(i)}.csv")
+            print("-"* 40)
 
+            print(f"Remove the file {fileName + str(i)}.csv")
+            
             df.to_csv(path + "/"+ dataFile)
 
-            print(f"Create {dataFile}")
+            print("-"* 40)
+
+            print(f"Create new file: {dataFile}")
 
         else:
             df.to_csv(path + "/"+ dataFile)
 
             print(f"Create {dataFile}")
 
-createBingo("cartones-bingo-75-bolas (18).pdf",os.getcwd(),"serie",9)
+createBingo("",os.getcwd(),"serie",9)
 
